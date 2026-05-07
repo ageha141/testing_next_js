7ca8bad9494f5340e6d73b36feef6f8e9484172a
ageha141 <ageha.mast11@gmail.com>
2026-05-08 04:39:43 +0900

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-// next.config.ts
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // images は experimental の中ではなく、ここ（一番上の階層）に書きます
-  images: {
-    unoptimized: true,
-  },
-  // basePath: '/ai-training-demo',
-  // assetPrefix: '/ai-training-demo/',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
